900c9e578c24dd6107c78312dd7b2f83a56f0ac8
Aslam Rizky <146598910+aslamrizky@users.noreply.github.com>
2025-03-03 15:01:54 +0700

    TPModul_3

---
+++ modul3/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace modul3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Kirim = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(526, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Kirim
+            // 
+            this.Kirim.Location = new System.Drawing.Point(598, 65);
+            this.Kirim.Name = "Kirim";
+            this.Kirim.Size = new System.Drawing.Size(148, 30);
+            this.Kirim.TabIndex = 1;
+            this.Kirim.Text = "Kirim";
+            this.Kirim.UseVisualStyleBackColor = true;
+            this.Kirim.Click += new System.EventHandler(this.Kirim_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Kirim);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Kirim;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
